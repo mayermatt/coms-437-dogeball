aeb39f6ab5eabd532bcbe32c78f03b69bae00662
mayermatt <mayer.matt36@gmail.com>
2018-03-31 20:31:10 -0500

    Publish-related changes

--- ComS437_MMayer_Pong/ComS437_MMayer_Pong/Properties/AssemblyInfo.cs
+++ ComS437_MMayer_Pong/ComS437_MMayer_Pong/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ComS437_MMayer_Pong")]
-[assembly: AssemblyProduct("ComS437_MMayer_Pong")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyTitle("Dogeball")]
+[assembly: AssemblyProduct("Dogeball")]
+[assembly: AssemblyDescription("Very Doge, So Game")]
+[assembly: AssemblyCompany("Matt Mayer")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
